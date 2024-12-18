6832663a8582a7684d4d58b03e5b96d513b5630e
hfoote <haydenfoote@arizona.edu>
2024-12-18 14:32:56 -0700

    Functional tests now check we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,9 @@
 # my version of the test suite with pytest
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
 import pytest
 
 # setup / teardown function for the browser
@@ -17,17 +20,28 @@ def test_can_start_a_list_and_retrieve_it_later(browser):
 
 	# and see that the page mentions to-do lists in its title
 	assert "To-Do" in browser.title
-	pytest.fail('Finish the test!')
+	header_text = browser.find_element(By.TAG_NAME, 'h1').text
+	assert "To-Do" in header_text
 
 	# The user is invited to enter a to-do list item
+	inputbox = browser.find_element(By.ID, 'id_new_item')
+	assert inputbox.get_attribute('placeholder') == 'Enter a to-do item'
 
 	# They enter "Buy replacement drum heads" into a textbox
+	inputbox.send_keys('Buy replacement drum heads')
 
 	# When they hit enter, the page updates and "1. Buy replacement drum heads" is 
 	# now listed as the first to-do list item
+	inputbox.send_keys(Keys.ENTER)
+	time.sleep(1)
+
+	table = browser.find_element(By.ID, 'id_list_table')
+	rows = table.find_elements(By.TAG_NAME, 'tr')
+	assert any(row.text == '1. Buy replacement drum heads' for row in rows)
 
 	# There is still a text box inviting them to add another item. They enter 
 	# "replace old drumheads"
+	pytest.fail('Finish the test!')
 
 	# The page updates again, and now shows both items on the user's list, along
 	# with another box to add more items
