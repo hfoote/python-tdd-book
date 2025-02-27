f8468fd64d0c691a1342f22e37866e6298646ed9
hfoote <haydenfoote@arizona.edu>
2025-02-27 13:18:23 -0700

    hack ALLOWED_HOSTS to be *

--- superlists/settings.py
+++ superlists/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-ixyc_ligcq4v7(u5dqnl#7#bgnx)d9yq*dtngg1ka@v#da_0-x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
