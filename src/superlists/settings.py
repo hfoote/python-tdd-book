1497a95cb24ad06f043c37f180ea79532a0d7cc8
hfoote <haydenfoote@arizona.edu>
2025-08-07 13:43:17 -0700

    email ssl certificate handling

--- src/superlists/settings.py
+++ src/superlists/settings.py
@@ -11,8 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-import os
+import os, certifi
 # from dotenv import load_dotenv
+
+# for email
+os.environ['SSL_CERT_FILE'] = certifi.where()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
